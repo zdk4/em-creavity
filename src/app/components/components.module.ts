cd3a391e52e882fcd01fd95d0d65c5fc1369954c
Sergio Castro <sergio.castro@bluestart.mx>
2022-03-22 13:45:58 -0600

    Cambios al formulario

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NouisliderModule } from 'ng2-nouislider';
 import { JwBootstrapSwitchNg2Module } from 'jw-bootstrap-switch-ng2';
 import { RouterModule } from '@angular/router';
@@ -17,7 +17,8 @@ import { CotizadorComponent } from './cotizador/cotizador.component';
         NgbModule,
         NouisliderModule,
         RouterModule,
-        JwBootstrapSwitchNg2Module
+        JwBootstrapSwitchNg2Module,
+        ReactiveFormsModule
     ],
     declarations: [
         ComponentsComponent,
--- src/app/components/cotizador/cotizador.component.ts
+++ src/app/components/cotizador/cotizador.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cotizador',
@@ -10,6 +11,28 @@ export class CotizadorComponent implements OnInit {
   constructor() { }
   public cotizadorWindow = false;
   public mostrarMasInformacion = false;
+  public nivelCobertura = {
+    basica: false,
+    estandar: false,
+    premium: false,
+    todas: false,
+  };
+
+  public formaPago = {
+    mensual: false,
+    trimestral: false,
+    semestral: false,
+    anual: false,
+  };
+
+  public registro = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    edad: new FormControl('', Validators.required),
+    estado: new FormControl('', Validators.required),
+    sexo: new FormControl('', Validators.required),
+    mail: new FormControl('', Validators.email),
+    tellefono: new FormControl('', Validators.required),
+  });
   ngOnInit(): void {
   }
 
@@ -17,4 +40,25 @@ export class CotizadorComponent implements OnInit {
     this.cotizadorWindow = true;
   }
   
+  cambioCobertura(cobertura: string) {
+    this.nivelCobertura = {
+      basica: false,
+      estandar: false,
+      premium: false,
+      todas: false,
+    };
+
+    this.nivelCobertura[cobertura] = true;
+  }
+
+  cambioFormaPago(pago: string) {
+    this.formaPago = {
+      mensual: false,
+      trimestral: false,
+      semestral: false,
+      anual: false,
+    };
+
+    this.formaPago[pago] = true;
+  }
 }
